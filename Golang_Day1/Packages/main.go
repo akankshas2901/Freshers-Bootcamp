1e1292789d9a329dd4c73cf25fb4162d3bba7afc
akankshas2901 <akanksha.solanki@razorpay.com>
2025-03-03 11:16:41 +0530

    Updated

--- Golang_Day1/Packages/main.go
+++ Golang_Day1/Packages/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	//random integer between 0 and 9.
+	fmt.Println("My favorite number is", rand.Intn(10))
+}
+
+
+// When importing a package, you can refer only to its exported names.Any "unexported" names are not accessible from outside the package.
+
+// Go has a rule:
+// If a function, variable, or constant starts with a lowercase letter, it is private (hidden) outside its package.
+// If it starts with a capital letter, it is public and can be used in other files.
+// ✔ Names starting with a capital letter (like Pi) can be used outside their package.
+// ❌ Names starting with a lowercase letter (like pi) are private and hidden.
